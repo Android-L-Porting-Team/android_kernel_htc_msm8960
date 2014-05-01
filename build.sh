5afc05f0298b71939b8eb3978e19120fa94a29b1
poondog <markj338@gmail.com>
2014-05-01 22:42:53 +1000

    BUILD: add Kangaroo Kernel buildscript
    Signed-off-by: poondog <markj338@gmail.com>

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Kangaroo Kernel build script thanks to CyanogenMod, anarkia1976
+
+# Kernel Version
+BASE_VER=".JOEY.CM"
+VER=".M7.001"
+VER=$BASE_VER$VER
+
+# AK Variables
+export LOCALVERSION=""`echo $VER`
+
+# toolchain
+export CROSS_COMPILE=${HOME}/Toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.8.3-2014.04/bin/arm-cortex_a15-linux-gnueabihf-
+
+# make .config
+make m7_defconfig
+env KCONFIG_NOTIMESTAMP=true \
+make ARCH=arm export SUBARCH=arm CROSS_COMPILE=arm-eabi-
+
+# build the kernel
+
+make -j4
