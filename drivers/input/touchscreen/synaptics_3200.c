5c59b56c9ff40ba168a074a0493eaf57a60d9f5d
poondog <markj338@gmail.com>
2014-05-24 13:34:58 +1000

    Revert "fix wakelock"
    This reverts commit 3811659a8285de54e4f7476b76d0d14336005522.

--- drivers/input/touchscreen/synaptics_3200.c
+++ drivers/input/touchscreen/synaptics_3200.c
@@ -253,8 +253,7 @@ static void sweep2wake_presspwr(struct work_struct * sweep2wake_presspwr_work) {
 
 	if (scr_suspended == true && pocket_detect == 1) {
 		if (pocket_detection_check()) {
-	        	if (wake_lock_active(&l2w_wakelock))
-				wake_unlock(&l2w_wakelock);
+	        	mutex_unlock(&pwrkeyworklock);
 			return;
 		}
 	}
@@ -332,7 +331,7 @@ static void logo2wake_longtap_count(struct work_struct * logo2wake_longtap_count
 	time_count = jiffies;
 	
 	if (scr_suspended)
-		wake_lock_timeout(&l2w_wakelock, HZ/3);
+		wake_lock_timeout(&l2w_wakelock, 35);
 
 	for (;;) {
 
@@ -2599,7 +2598,7 @@ static void dt2w_func(int x, int y, cputime64_t trigger_time)
                         wakesleep_vib = 1;
                         reset_dt2w();
 			//printk("dt2w ON\n");
-			wake_lock_timeout(&l2w_wakelock, HZ/2);
+			wake_lock(&l2w_wakelock);
                         sweep2wake_pwrtrigger();
                 } else {
 			prev_time = trigger_time;
