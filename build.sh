a9b3e31804aec0b1a3d7d5a05dffbb4b9d6f0306
poondog <markj338@gmail.com>
2014-08-11 18:05:34 +1000

    BUILD: L port
    Please see comment for the toolchain line if you are compiling yourself

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 # Kangaroo Kernel build script thanks to CyanogenMod, anarkia1976
 
 # Kernel Version
-BASE_VER=".JOEY.CM"
+BASE_VER=".JOEY.L.port"
 VER=".M7"
 VER=$BASE_VER$VER
 
 # AK Variables
 export LOCALVERSION=""`echo $VER`
 
-# toolchain
+# toolchain: put your toolchain here, but don't commit it, I don't want everyone changing it to their own lol
 export CROSS_COMPILE=${HOME}/Toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.06/bin/arm-cortex_a15-linux-gnueabihf-
 
 # make .config
